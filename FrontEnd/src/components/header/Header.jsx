d53eb698fe1f5c709f9ff762c7a6820b45fb5d0b
Rapheal James <raphealjunior07@gmail.com>
2023-07-19 15:37:41 +0100

    workin okay

--- FrontEnd/src/components/header/Header.jsx
+++ FrontEnd/src/components/header/Header.jsx
@@ -1,89 +1,12 @@
-import React, { useState } from 'react'
-import "./HeaderStyle.css"
-import {AiOutlineSearch, 
-    AiFillHeart, 
-    AiOutlineShoppingCart,
-    AiOutlineUserAdd
-} from "react-icons/ai"
-import {useSelector } from 'react-redux'
-import { Link, useNavigate} from 'react-router-dom'
-import { signOut } from '../Redux/ProductState'
-import { useDispatch } from 'react-redux';
-
+import React from "react";
 
 const Header = () => {
-    const nav = useNavigate()
-    const Dispatch = useDispatch()
-    const [showLogInfo, setShowLogInfo] = useState(false)
-    const Cart = useSelector((state)=> state.persisitedReducer.addcart)
-    const user = useSelector((state)=> state.persisitedReducer.user)
-    console.log(user)
-
-  return (
-    <div className='HeaderBody'>
-        <div className="HeaderWrapper">
-            <h2>Eflex <span>Shop</span></h2>
-            <nav>
-                <ul>
-                    <Link to="/" style={{color: "black"}}>
-                    <li>Home</li>
-                    </Link>
-                    <Link to="/product" style={{color: "black"}}>
-                    <li>Product</li>
-                    </Link>
-                    <Link to="/service" style={{color: "black"}}>
-                    <li>Service</li>
-                    </Link>
-                    <Link to="/contact" style={{color: "black"}}>
-                    <li>Contact</li>
-                    </Link>
-
-                    {
-                        user?.isAdmin? <Link to={`/product/${user.token}/${user._id}`} style={{color: "black"}}>
-                        <li>Upload Product</li>
-                        </Link> : null
-                    }
-                    
-                </ul>
-            </nav>
-
-            <div className='HeaderIcons'>
-                <AiOutlineSearch className='HeaderIconsIcon'/>
-                <AiFillHeart className='HeaderIconsIcon'/>
-               <div className='cartIcon' onClick={()=> nav("/cartpage")}>
-               <AiOutlineShoppingCart className='HeaderIconsIcon ' onClick={()=> nav("/cartpage")}/>
-                <div className='cartIconBox'>{user?<>{Cart.length}</> : 0}</div>
-               </div>
-               {
-                user? <div className='UserBigBox' onMouseOver={()=>setShowLogInfo(!showLogInfo)}>
-                       {user.name?.charAt(0)} 
-                </div> : <AiOutlineUserAdd className='HeaderIconsIcon' onMouseOver={()=>setShowLogInfo(!showLogInfo)}/>
-               }
-              {
-                showLogInfo?  
-                <div className='Reg' onMouseLeave={()=> setShowLogInfo(false)}>
-                    <ul className='RegUl'>
-                        {
-                            user? <>
-                                <li onClick={()=> Dispatch(signOut())}>Sign out</li>
-                            </> : 
-                            <>
-                            <Link to="/login" style={{color: "black"}}>
-                            <li>Log in</li>
-                        </Link>
-                        <Link to="/signup" style={{color: "black"}}>
-                        <li>Sign up</li>
-                        </Link>
-                            </>
-                        }
-                    </ul>
-                </div> : null
-              }
-            </div>
-
+    return (
+        <div>
+            <h1> my name is victor </h1>
+            <p className="lead"> i am a web developer and designer</p>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Header
+export default Header;
