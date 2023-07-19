199e522711d21b17cdfebfcfc02cb87c655ef9ee
EbukaNweje <nwejeebuka@gmail.com>
2023-07-19 08:03:09 -0700

    Working on it

--- FrontEnd/src/components/header/Header.jsx
+++ FrontEnd/src/components/header/Header.jsx
@@ -5,6 +5,9 @@ const Header = () => {
         <div>
             <h1> my name is victor </h1>
             <p className="lead"> i am a web developer and designer</p>
+            <div>
+              <input type="text" />
+            </div>
         </div>
     );
 };
