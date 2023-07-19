a14061760a754ccfe95c169fe90e05ca0314f830
Rapheal James <raphealjunior07@gmail.com>
2023-07-19 15:52:10 +0100

    Good

--- FrontEnd/src/components/header/Header.jsx
+++ FrontEnd/src/components/header/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
         <div>
             <h1> my name is victor </h1>
             <p className="lead"> i am a web developer and designer</p>
+            <h1>The boy is good</h1>
         </div>
     );
 };
